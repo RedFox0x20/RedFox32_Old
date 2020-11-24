acc23a64590c42972add4dd464cee958b7320b49
RedFox0x20 <iris@redfox32.xyz>
2020-11-24 13:14:42 +0000

    Fixed some formatting

--- RedFox32/Source/BootloaderStage2/Stage2A.asm
+++ RedFox32/Source/BootloaderStage2/Stage2A.asm
@@ -145,8 +145,9 @@ LoadKernel:
 	ret
 
 ; use the INT 0x15, eax= 0xE820 BIOS function to get a memory map
-; note: initially di is 0, be sure to set it to a value so that the BIOS code will not be overwritten. 
-;       The consequence of overwriting the BIOS code will lead to problems like getting stuck in `int 0x15`
+; note: initially di is 0, be sure to set it to a value so that the BIOS
+;       code will not be overwritten. The consequence of overwriting the BIOS
+;       code will lead to problems such as getting stuck in `int 0x15`
 ; inputs: es:di -> destination buffer for 24 byte entries
 ; outputs: bp = entry count, trashes all registers except esi
 ; CODE FROM:
@@ -155,49 +156,53 @@ mmap_ent equ 0x8000             ; the number of entries will be stored at 0x8000
 ; 0x8000 = uint32 num entries
 ; 0x8004+ = Entries see mmap_qemu.txt
 CreateMemoryMap:
-    mov di, 0x8004          ; Set di to 0x8004. Otherwise this code will get stuck in `int 0x15` after some entries are fetched 
-	xor ebx, ebx		; ebx must be 0 to start
-	xor bp, bp		; keep an entry count in bp
+	; Set di to 0x8004. Otherwise this code will get stuck in
+	; `int 0x15` after some entries are fetched 
+    mov di, 0x8004
+	xor ebx, ebx			; ebx must be 0 to start
+	xor bp, bp				; keep an entry count in bp
 	mov edx, 0x0534D4150	; Place "SMAP" into edx
 	mov eax, 0xe820
 	mov [di + 20], dword 1	; force a valid ACPI 3.X entry
-	mov ecx, 24		; ask for 24 bytes
+	mov ecx, 24				; ask for 24 bytes
 	int 0x15
-	jc short .failed	; carry set on first call means "unsupported function"
+	jc short .failed		; carry set on first call means "unsupported function"
 	mov edx, 0x0534D4150	; Some BIOSes apparently trash this register?
-	cmp eax, edx		; on success, eax must have been reset to "SMAP"
+	cmp eax, edx			; on success, eax must have been reset to "SMAP"
 	jne short .failed
-	test ebx, ebx		; ebx = 0 implies list is only 1 entry long (worthless)
+	test ebx, ebx			; ebx = 0 implies list is only 1 entry long
+							; A single entry here is useless information
 	je short .failed
 	jmp short .jmpin
 .e820lp:
-	mov eax, 0xe820		; eax, ecx get trashed on every int 0x15 call
+	mov eax, 0xe820			; eax, ecx get trashed on every int 0x15 call
 	mov [di + 20], dword 1	; force a valid ACPI 3.X entry
-	mov ecx, 24		; ask for 24 bytes again
+	mov ecx, 24				; ask for 24 bytes again
 	int 0x15
-	jc short .e820f		; carry set means "end of list already reached"
+	jc short .e820f			; carry set means "end of list already reached"
 	mov edx, 0x0534D4150	; repair potentially trashed register
 .jmpin:
-	jcxz .skipent		; skip any 0 length entries
-	cmp cl, 20		; got a 24 byte ACPI 3.X response?
+	jcxz .skipent			; skip any 0 length entries
+	cmp cl, 20				; got a 24 byte ACPI 3.X response?
 	jbe short .notext
 	test byte [di + 20], 1	; if so: is the "ignore this data" bit clear?
 	je short .skipent
 .notext:
-	mov ecx, [di + 8]	; get lower uint32_t of memory region length
-	or ecx, [di + 12]	; "or" it with upper uint32_t to test for zero
-	jz .skipent		; if length uint64_t is 0, skip entry
-	inc bp			; got a good entry: ++count, move to next storage spot
+	mov ecx, [di + 8]		; get lower uint32_t of memory region length
+	or ecx, [di + 12]		; "or" it with upper uint32_t to test for zero
+	jz .skipent				; if length uint64_t is 0, skip entry
+	inc bp					; got a good entry: ++count, move to next storage spot
 	add di, 24
 .skipent:
-	test ebx, ebx		; if ebx resets to 0, list is complete
+	test ebx, ebx			; if ebx resets to 0, list is complete
 	jne short .e820lp
 .e820f:
-	mov [mmap_ent], bp	; store the entry count
-	clc			; there is "jc" on end of list to this point, so the carry must be cleared
+	mov [mmap_ent], bp		; store the entry count
+	clc						; there is "jc" on end of list to this point,
+							; so the carry must be cleared
 	ret
 .failed:
-	stc			; "function unsupported" error exit
+	stc						; "function unsupported" error exit
 	ret
 
 ;******************************************************************************
