668c364dca58b9c8b9e4d1f9908df63566752880
RedFox0x20 <iris@redfox32.xyz>
2020-12-02 20:07:26 +0000

    Keyboard driver
    Implemented a basic keyboard driver which is capable of handling all
    regular keys following the US QWERTY (the scancodes are the same for GB
    keyboards in my case) 0-9, a-z and basic symbols.
    
    The driver still needs the ability for control keys to be implemented in
    addition for a method of getting a key when it's pressed on demand
    (reference getch).
    
    The keyboard layout can be configured using a header file in
    RedFox32/Source/Kernel/KeyboardLayouts/<LayoutName>.h ensuring that all
    header guards and variables are named exactly the same. Matching header
    guards ensures that only the first layout is used and naming ensures no
    code will break.
    
    Old source has been deleted as it is no longer required for referencing.

--- RedFox32/Source/Kernel/Keyboard.c
+++ RedFox32/Source/Kernel/Keyboard.c
@@ -2,15 +2,23 @@
 #include <Kernel/IDT.h>
 #include <Kernel/TextMode.h>
 #include <Kernel/Keyboard.h>
+#include <Kernel/KeyboardLayouts/GB.h>
 
 void Keyboard_Handler(void)
 {
 	unsigned char Keycode = inb(KEYBOARD_DATA_PORT);
-	unsigned char State = Keycode & 0x80;
 	unsigned char Extended = (Keycode == 0xE0) ? 1 : 0;
 	if (Extended)
 	{
-		Keycode = inb(KEYBOARD_DATA_PORT);
+		Keycode = inb(KEYBOARD_DATA_PORT);	
+	}
+	unsigned char State = Keycode & 0x80 ? KEY_STATE_UP : KEY_STATE_DOWN;
+	/* If they key is up then bit 0x80 is set so we can remove it to get the
+	 * normal scancode
+	 */
+	if (State == KEY_STATE_UP)
+	{
+		Keycode -= 0x80;
 	}
 
 	puts("[KEYBOARD ", 0x0A);
@@ -20,6 +28,8 @@ void Keyboard_Handler(void)
 	putch_hex(Keycode, 0x0A);
 	puts(" State=0x", 0x0A);
 	putch_hex(State, 0x0A);
+	puts(" Char=", 0x0A);
+	putch(KeyboardLayout[Keycode], 0x0A);
 	puts("]\n", 0x0A);
 }
 
--- RedFox32/Source/Kernel/Keyboard.h
+++ RedFox32/Source/Kernel/Keyboard.h
@@ -2,6 +2,8 @@
 #define KERNEL_KEYBOARD_H
 
 #define KEYBOARD_DATA_PORT 0x60
+#define KEY_STATE_UP 0
+#define KEY_STATE_DOWN 1
 
 void Keyboard_Setup(void);
 
