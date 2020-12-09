ecaae9b1f3fa4aa6ac14a3387cfc9c2d9f42260f
RedFox0x20 <iris@redfox32.xyz>
2020-12-09 13:53:19 +0000

    outw

--- RedFox32/Source/Kernel/IO.h
+++ RedFox32/Source/Kernel/IO.h
@@ -2,6 +2,7 @@
 #define KERNEL_STAGE2_IO_H
 
 void outb(unsigned short port, unsigned char val);
+void outw(unsigned short port, unsigned short val);
 unsigned char inb(unsigned short port);
 
 #endif
